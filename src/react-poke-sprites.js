7dcef5825a6cb2219c5f7519672c5fce25b3950c
Anshuman <anshu.av97@gmail.com>
2018-01-15 10:00:16 +0530

    Add pokemon ID support

--- src/react-poke-sprites.js
+++ src/react-poke-sprites.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import pokemonGif from 'pokemon-gif';
 import pokemonPropType from './pokemon-prop';
+import pokemonNumberToName from 'pokemon-gif/lib/pokedex-number-to-name';
 
 class PokeSprite extends React.Component {
 	render() {
 		const { pokemon, className } = this.props;
+		pokemon = typeof pokemon === 'string' ? pokemon : pokemonNumberToName[pokemon];
 		return (
 			<img
 				src={pokemonGif(pokemon)}
