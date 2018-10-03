2600cc2c08e21df74df599b8177fa5afb145e94b
Jeison Higuita Sanchez <jeisonhs93@gmail.com>
2018-10-03 08:37:14 -0500

    Update react-poke-sprites.js

--- src/react-poke-sprites.js
+++ src/react-poke-sprites.js
@@ -5,12 +5,29 @@ import PropTypes from 'prop-types';
 import pokemonNumberToName from 'pokemon-gif/lib/pokedex-number-to-name';
 
 class PokeSprite extends React.Component {
+	get gifUrl () {
+		const { pokemon, className } = this.props;
+		const name = typeof pokemon === 'string' ? pokemon : pokemonNumberToName[pokemon];
+		let url = '';
+		try {
+			url = pokemonGif(name);
+		} catch (e) {
+			console.log(e);
+			url = '';
+		}
+		return url;
+	}
 	render() {
 		const { pokemon, className } = this.props;
 		const name = typeof pokemon === 'string' ? pokemon : pokemonNumberToName[pokemon];
+		
+		if (!this.gifUrl) {
+			return null;
+		}
+		
 		return (
 			<img
-				src={pokemonGif(name)}
+				src={this.gifUrl}
 				alt={name}
 				className={className}
 			/>
